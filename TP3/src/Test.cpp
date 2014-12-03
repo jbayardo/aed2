687290c2ba20118d0899434a51972ad4714a2b28
Izikiel <ezequiel.gambaccini@gmail.com>
2014-12-02 21:51:44 -0300

    Pasa valgrind bien

--- TP3/src/Test.cpp
+++ TP3/src/Test.cpp
@@ -152,7 +152,6 @@ int main(int argc, char **argv)
     const std::string word_list[] = {"about","after","again","air","all","along","also","an","and","another","any","are","around","as","at","away","back","be","because","been","before","below","between","both","but","by","came","can","come","could","day","did","different","do","does","do't","down","each","end","even","every","few","find","first","for","found","from","get","give","go","good","great","had","has","have","he","help","her","here","him","his","home","house","how","I","if","in","into","is","it","its","just","know","large","last","left","like","line","little","long","look","made","make","man","many","may","me","men","might","more","most","Mr.","must","my","name","never","new","next","no","not","now","number","of","off","old","on","one","only","or","other","our","out","over","own","part","people","place","put","read","right","said","same","saw","say","see","she","should","show","small","so","some","something","sound","still","such","take","tell","than","that","the","them","then","there","these","they","thing","think","this","those","thought","three","through","time","to","together","too","two","under","up","us","use","very","want","water","way","we","well","went","were","what","when","where","which","while","who","why","will","with","word","work","world","would","write","year","you","your","was",};
 
     DiccString<int>* dictest = new DiccString<int>();
-
     for (int i = 0; i < word_list->length(); ++i) {
         dictest->Definir(word_list[i], i);
         ASSERT(dictest->Definido(word_list[i]));
@@ -160,7 +159,16 @@ int main(int argc, char **argv)
     }
     delete dictest;
 
-    std::cout << "Todo bien :D" << std::endl;
+    DiccString<std::string>* dic_test_string = new DiccString<std::string>();
+    for (int i = 0; i < word_list->length(); ++i) {
+        dic_test_string->Definir(word_list[i], word_list[i]);
+        ASSERT(dic_test_string->Definido(word_list[i]));
+        ASSERT_EQ(dic_test_string->Obtener(word_list[i]), word_list[i]);
+    }
+    delete dic_test_string;
+
+    std::cout << "Test terminado" << std::endl;
+
 
     return 0;
 }
